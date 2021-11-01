1fc836ffc732c81f9c2fd3e0b589315ada7c62c0
MattEveritt <email@example.com>
2021-11-01 14:58:58 +0200

    update

--- cypress/integration/note_app.js
+++ cypress/integration/note_app.js
@@ -1,6 +1,6 @@
 describe('Pokedex', function () {
   it('front page can be opened', function () {
-    cy.visit('http://localhost:8080')// eslint-disable-line
+    cy.visit('http://localhost:5000')// eslint-disable-line
     cy.contains('ivysaur')// eslint-disable-line
     cy.contains(// eslint-disable-line
       'Pokémon and Pokémon character names are trademarks of Nintendo.'
